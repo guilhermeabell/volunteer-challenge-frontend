d1ee510cf5847782e900b9c9e3ff3bb55e8feeed
guilhermeabell <gpeuvis@gmail.com>
2023-05-31 01:42:07 -0300

    feat: adding UserPerson and Professional page

---
+++ src/pages/Professional/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import Ilustration from '../../assets/professional.svg'
+import Header from '../../fragments/Header'
+import Main from '../../fragments/Main'
+import Footer from '../../fragments/Footer'
+
+const Professional: React.FC = () => {
+
+  return (
+    <>
+    <Header />
+    <Main
+    imageSrc={Ilustration}
+    imageAlt="Professional
+    image"
+    title="Profissional" 
+    subtitle="A Lacrei garante que pessoas LGBTQIAPN + <br /> recebam atendimento realizado por profissionais <br /> de qualidade e que atendam às suas <br /> necessidades de forma segura e acolhedora." 
+    />
+    <Footer />
+    </>
+  )
+}
+
+export default Professional
---
+++ src/pages/UserPerson/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import Ilustration from '../../assets/userPerson.svg'
+import Header from '../../fragments/Header'
+import Main from '../../fragments/Main'
+import Footer from '../../fragments/Footer'
+
+const UserPerson: React.FC = () => {
+
+  return (
+    <>
+    <Header />
+    <Main
+    imageSrc={Ilustration}
+    imageAlt="UserPerson
+    image"
+    title="Pessoa Usuária" 
+    subtitle="A Lacrei garante que pessoas LGBTQIAPN + <br /> recebam atendimento realizado por profissionais <br /> de qualidade e que atendam às suas <br /> necessidades de forma segura e acolhedora." 
+    />
+    <Footer />
+    </>
+  )
+}
+
+export default UserPerson
