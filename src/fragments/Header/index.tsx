76be09c6127dbe0783cdfe58faf18615bae04fa3
guilhermeabell <gpeuvis@gmail.com>
2023-05-30 22:45:55 -0300

    feat: adding Header component

---
+++ src/fragments/Header/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+import * as S from './styles';
+
+const Header = () => {
+  return (
+    <S.HeaderContainer>
+      <S.Title>Lacrei</S.Title>
+      <S.NavigationMenu>
+        <S.NavigationMenuItem>
+           <a href="/">Home</a>
+        </S.NavigationMenuItem>
+        <S.NavigationMenuItem>
+          Pessoa Usuária
+        </S.NavigationMenuItem>
+        <S.NavigationMenuItem>
+          Profissional
+        </S.NavigationMenuItem>
+      </S.NavigationMenu>
+    </S.HeaderContainer>
+  );
+};
+
+export default Header;
---
+++ src/fragments/Header/styles.ts
@@ -0,0 +1,43 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 1.25rem;
+  height: 5rem;
+  background-color: #EEEEEE;
+`
+
+export const Title = styled.h1`
+font-size: 2rem;
+padding: 2rem;
+font-weight: bold;
+line-height: 44px;
+color: #018762;
+`
+export const NavigationMenu = styled.ul`
+  display: flex;
+  color: #1F1F1F;
+  list-style: none;
+  font-weight: bold;
+  gap: 2.8rem;
+  padding: 2rem;
+`;
+
+export const NavigationMenuItem = styled.li`
+  margin-left: 16px;
+  cursor: pointer;
+
+  &:first-child {
+    margin-left: 0;
+  }
+   a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  &:hover {
+      color: #018762;
+    }
+`;
