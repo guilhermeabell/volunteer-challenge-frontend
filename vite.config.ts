8fa071ea834c9dd555bf10111fea2e33192f6e8d
guilhermeabell <gpeuvis@gmail.com>
2023-06-02 09:36:51 -0300

    chore: created aliases paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
+
 import react from '@vitejs/plugin-react'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@fragments': path.resolve(__dirname, 'src/fragments'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@app': path.resolve(__dirname, 'src/app'),
+    },
   },
 })
